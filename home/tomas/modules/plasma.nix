3580b0320c887d6a24d4ab21a1034dd99c933aa1
Tomas <vojtisektomas92@gmail.com>
2026-06-13 17:16:42 +0200

    set breeze dark

--- home/tomas/modules/plasma.nix
+++ home/tomas/modules/plasma.nix
@@ -1,3 +1,16 @@
 { ... }: {
+  programs.plasma = {
+    enable = true;
 
+    workspace = {
+      lookAndFeel = "org.kde.breezedark.desktop";
+    };
+
+    hotkeys.commands."launch-konsole" = {
+      name = "Launch Konsole";
+      key = "Meta+Return";
+      command = "konsole";
+    };
+
+  };
 }
