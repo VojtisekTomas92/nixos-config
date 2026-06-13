3a198faca31ac5579358cbba13ef9a2d3390c1b3
Tomas <vojtisektomas92@gmail.com>
2026-06-13 13:48:08 +0200

    Update nixcord.nix

--- home/tomas/modules/nixcord.nix
+++ home/tomas/modules/nixcord.nix
@@ -1,16 +1,28 @@
 { pkgs, ... }: {
   programs.nixcord = {
     enable = true;
+
+    discord.vencord.enable = false;
     discord.equicord.enable = true;
 
-    discord.package = pkgs.discord.override {
-      extraEnv = {
-        __NV_PRIME_RENDER_OFFLOAD = "1";
-        __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-        __VK_LAYER_NV_optimus = "NVIDIA_only";
+    discord.package = pkgs.discord.overrideAttrs (oldAttrs: {
+      passthru = (oldAttrs.passthru or { }) // {
+        override =
+          args:
+          pkgs.symlinkJoin {
+            name = "discord-nvidia-offload";
+            paths = [ pkgs.discord ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            postBuild = ''
+              wrapProgram $out/bin/discord \
+                --set __NV_PRIME_RENDER_OFFLOAD "1" \
+                --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER "NVIDIA-G0" \
+                --set __GLX_VENDOR_LIBRARY_NAME "nvidia" \
+                --set __VK_LAYER_NV_optimus "NVIDIA_only"
+            '';
+          };
       };
-    };
+    });
 
   };
 }
