f8d329705fc829205666a78e271bfa2ef4cdff7a
Tomas <vojtisektomas92@gmail.com>
2026-06-17 15:53:32 +0200

    Confihure kde settings

--- home/tomas/modules/plasma.nix
+++ home/tomas/modules/plasma.nix
@@ -6,10 +6,38 @@
       lookAndFeel = "org.kde.breezedark.desktop";
     };
 
+    input = {
+      keyboard = {
+        numlockOnStartup = "on";
+        layouts = [
+          {
+            layout = "de";
+          }
+        ];
+      };
+    };
+
     hotkeys.commands."launch-konsole" = {
       name = "Launch Konsole";
       key = "Meta+Return";
       command = "konsole";
+    };
+
+    session = {
+      sessionRestore = {
+        restoreOpenApplicationsOnLogin = "startWithEmptySession";
+      };
+    };
+
+    powerdevil = {
+      AC = {
+        autoSuspend = {
+          action = "sleep";
+          idleTimeout = 7200;
+        };
+        whenLaptopLidClosed = "shutDown";
+        inhibitLidActionWhenExternalMonitorConnected = false;
+      };
     };
 
   };
